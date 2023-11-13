c8a15b55b1ff2ad16f73c2b6c3db36baeca1db89
fabio <fabio@digitalmind.com>
2023-11-13 00:03:25 -0300

    some projects

--- renderizador_proyectos.js
+++ renderizador_proyectos.js
@@ -1,27 +1,57 @@
 const proyectos = [
   {
-    title: 'Game Shop',
-    imgUrl: 'img/game.png',
-    url: 'https://github.com/PabloNMerino/Maquetados---Front-End-1-/tree/main/GameShop',
-    descripcion: 'bolsa de juegos'
+    title: 'CV-Batman',
+    imgUrl: 'img/batman.webp',
+    url: 'https://fabiodrizzt.github.io/CV-Batman/',
+    descripcion: 'CV-Batman'
   },
   {
-    title: 'Examen final FrontEnd 1',
-    imgUrl: 'img/logo.svg',
-    url: 'https://github.com/PabloNMerino/Maquetados---Front-End-1-/tree/main/Manage',
-    descripcion: 'logo manage'
+    title: 'HTML Forms',
+    imgUrl: 'img/htmlform.png',
+    url: 'https://fabiodrizzt.github.io/Formularios-en-HTML/',
+    descripcion: 'Formularios HTML'
+  },
+  {
+    title: 'Animaciones',
+    imgUrl: 'img/animaciones.gif',
+    url: 'https://fabiodrizzt.github.io/Animaciones/',
+    descripcion: 'Animaciones'
+  },
+  {
+    title: 'PseudoSelectores',
+    imgUrl: 'img/PseudoSelectores.webp',
+    url: 'https://fabiodrizzt.github.io/PseudoSelectores/',
+    descripcion: 'PseudoSelectores'
+  },
+  {
+    title: 'Pizza&Front',
+    imgUrl: 'img/pizza.png',
+    url: 'https://fabiodrizzt.github.io/PizzaandFront/',
+    descripcion: 'Pizza&Front'
+  },
+  {
+    title: 'Super Heroes',
+    imgUrl: 'img/superheroes.png',
+    url: 'https://fabiodrizzt.github.io/SuperHeroes/',
+    descripcion: 'Super Heroes'
+  },
+  {
+    title: 'Game Shop',
+    imgUrl: 'img/game.png',
+    url: 'https://fabiodrizzt.github.io/GameShop/',
+    descripcion: 'bolsa de juegos'
   },
   {
     title: 'Pet Shop',
     imgUrl: 'img/pet-shop.png',
-    url: 'https://github.com/PabloNMerino/Maquetados---Front-End-1-/tree/main/PetShop',
+    url: 'hhttps://fabiodrizzt.github.io/PetShop/',
     descripcion: 'logo pet shop'
   },
   {
     title: 'Instafront',
     imgUrl: 'img/instafront.png',
-    url: 'https://github.com/PabloNMerino/Maquetados---Front-End-1-/tree/main/instafront',
-    descripcion: 'logo usuario instafront'
+    url: 'https://fabiodrizzt.github.io/InstaFront/',
+    descripcion: 'logo instafront'
   }
 ]
 
